c5927efa56cf69d1e187947c5ec21aeeeb1ae7b5
徐伟智 <624244232@qq.com>
2020-12-24 00:16:11 +0800

    Kafka contanier

---
+++ kafka/init.sh
@@ -0,0 +1,8 @@
+rm -rf data
+#is_create_network=$(docker network list | grep app-tier | wc -l)
+#if [ ${is_create_network} -eq 0 ]; then
+#  docker network create app-tier --driver bridge
+#fi
+for i in {1..3}; do
+  mkdir -p $(pwd)/data/kafka${i} && chmod -R 777 $(pwd)/data/kafka${i}
+done

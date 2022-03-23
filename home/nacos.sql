dd8b45d714f155c01c46228f7692cdfe176af1ed
xuweizhi <hanlin.xwz@come-future.com>
2022-03-23 19:44:57 +0800

    feat: es 降级

--- home/nacos.sql
+++ home/nacos.sql
@@ -17,6 +17,10 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE IF NOT EXISTS `nacos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+
+use nacos;
+
 -- ----------------------------
 -- Table structure for config_info
 -- ----------------------------

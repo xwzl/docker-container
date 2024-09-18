6ddf9edff4bbad397babac2e86bcadb665572d00
xuweizhi <624244232@qq.com>
2024-09-18 09:45:02 +0800

    grafana prometheus

--- home/init.sh
+++ home/init.sh
@@ -50,6 +50,21 @@ echo -e "${GREEN_COLOR}---> create [nacos]directory start.${RES}"
 if [ ! -d "./nacos/"  ]; then
 mkdir -p ./nacos/conf/
 fi
+
+echo -e "${GREEN_COLOR}---> create [prometheus]directory start.${RES}"
+if [ ! -d "./prometheus/"  ]; then
+mkdir -p ./prometheus/conf/
+mkdir -p ./prometheus/data/
+fi
+
+
+echo -e "${GREEN_COLOR}---> create [grafana]directory start.${RES}"
+if [ ! -d "./grafana/"  ]; then
+mkdir -p ./grafana/conf/
+mkdir -p ./grafana/data/
+fi
+
+
 echo -e "${BLUE_COLOR}===> create directory success.${RES}"
 
 # 目录授权(data/logs 都要授读/写权限)
@@ -71,11 +86,11 @@ fi
 echo -e "${BLUE_COLOR}===> directory authorize success.${RES}"
 
 # 移动配置文件
-echo -e "${BLUE_COLOR}---> move [elasticsearch]config file start.${RES}"
+echo -e "${BLUE_COLOR}---> copy [elasticsearch]config file start.${RES}"
 if [ -f "./es-master.yml" ] && [ -f "./es-slave1.yml" ] && [ -f "./es-slave2.yml" ]; then
-mv ./es-master.yml ./elasticsearch/master/conf
-mv ./es-slave1.yml ./elasticsearch/slave1/conf
-mv ./es-slave2.yml ./elasticsearch/slave2/conf
+cp ./es-master.yml ./elasticsearch/master/conf
+cp ./es-slave1.yml ./elasticsearch/slave1/conf
+cp ./es-slave2.yml ./elasticsearch/slave2/conf
 cp ./elastic-certificates.p12 ./elasticsearch/master
 cp ./elastic-certificates.p12 ./elasticsearch/slave1
 cp ./elastic-certificates.p12 ./elasticsearch/slave2
@@ -84,40 +99,51 @@ cp -r ./ik ./elasticsearch/slave1/plugins
 cp -r ./ik ./elasticsearch/slave2/plugins
 fi
 
-echo -e "${RED_COLOR}---> move [kibana]config file start.${RES}"
+echo -e "${RED_COLOR}---> copy [kibana]config file start.${RES}"
 if [ -f "./kibana.yml" ]; then
-mv ./kibana.yml ./kibana/conf
+cp ./kibana.yml ./kibana/conf
 fi
 
-echo -e "${GREEN_COLOR}---> move [logstash]config file start.${RES}"
+echo -e "${GREEN_COLOR}---> copy [logstash]config file start.${RES}"
 if [ -f "./logstash.conf" ]; then
-mv ./logstash.conf ./logstash/conf
-mv ./logstash.yml ./logstash/yml
+cp ./logstash.conf ./logstash/conf
+cp ./logstash.yml ./logstash/yml
 fi
 
-echo -e "${GREEN_COLOR}---> move [filebeat]config file start.${RES}"
+echo -e "${GREEN_COLOR}---> copy [filebeat]config file start.${RES}"
 if [ -f "./filebeat.yml" ]; then
-mv ./filebeat.yml ./filebeat/conf
+cp ./filebeat.yml ./filebeat/conf
 fi
 
-echo -e "${VIOLET_COLOR}---> move [nginx]config file start.${RES}"
+echo -e "${VIOLET_COLOR}---> copy [nginx]config file start.${RES}"
 if [ -f "./nginx.conf" ]; then
-mv ./nginx.conf ./nginx/conf
+cp ./nginx.conf ./nginx/conf
 fi
 
-echo -e "${VIOLET_COLOR}---> move [mysql]config file start.${RES}"
+echo -e "${VIOLET_COLOR}---> copy [mysql]config file start.${RES}"
 if [ -f "./my.cnf" ]; then
-mv ./my.cnf ./mysql/conf
+cp ./my.cnf ./mysql/conf
 fi
 
-echo -e "${VIOLET_COLOR}---> move [nacos]config file start.${RES}"
+echo -e "${VIOLET_COLOR}---> copy [nacos]config file start.${RES}"
 if [ -f "./nacos.properties" ]; then
-mv ./nacos.properties ./nacos/conf/
-mv ./nacos.sql ./mysql/init/
-mv ./nacos.env ./nacos/conf/
+cp ./nacos.properties ./nacos/conf/
+cp ./nacos.sql ./mysql/init/
+cp ./nacos.env ./nacos/conf/
 fi
 
-echo -e "${BLUE_COLOR}===> move config files success.${RES}"
+echo -e "${VIOLET_COLOR}---> copy [grafana]config file start.${RES}"
+if [ -f "./grafana.ini" ]; then
+cp ./grafana.ini ./grafana/conf/
+fi
+
+echo -e "${VIOLET_COLOR}---> copy [prometheus]config file start.${RES}"
+if [ -f "./prometheus.yml" ]; then
+cp ./prometheus.yml ./prometheus/conf/
+fi
+
+
+echo -e "${BLUE_COLOR}===> copy config files success.${RES}"
 
 echo -e "${GREEN_COLOR}>>>>>>>>>>>>>>>>>> The End <<<<<<<<<<<<<<<<<<${RES}"
 
